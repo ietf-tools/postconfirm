8f0495d0d24ce35b553fb03ee71beda99784cff6
Henrik Levkowetz <henrik@levkowetz.com>
2009-05-30 22:37:38 +0000

    Rev 0.29:   * The timeout error messages in sockserver.py used a nonexistent function     and never made it out.  Fixed.
    Legacy-Id: 14

--- postconfirmd.py
+++ postconfirmd.py
@@ -61,7 +61,7 @@ import StringIO
 # ------------------------------------------------------------------------------
 # Misc. metadata
 
-version = "0.28"
+version = "0.29"
 program = os.path.basename(sys.argv[0])
 progdir = os.path.dirname(sys.argv[0])
 
