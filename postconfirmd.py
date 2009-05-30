8a8d85dbe4ed3a5880cbca0212923612475d911a
Henrik Levkowetz <henrik@levkowetz.com>
2009-05-30 22:37:38 +0000

    Rev 0.31:   * Removed duplicate log messages (when TimeoutError was raised).
      * Tweaked the wrapper script to not be sensitive to the domain name
        when comparing -owner@ and -bounces@ addresses.
     - Legacy-Id: 16

--- postconfirmd.py
+++ postconfirmd.py
@@ -61,7 +61,7 @@ import StringIO
 # ------------------------------------------------------------------------------
 # Misc. metadata
 
-version = "0.30"
+version = "0.31"
 program = os.path.basename(sys.argv[0])
 progdir = os.path.dirname(sys.argv[0])
 
