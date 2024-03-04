c2098d59e87aced820c151be43940bb90607a349
Alistair MacDonald <Alistair.MacDonald@siriusopensource.com>
2024-03-04 14:25:53 +0000

    refactor: Split out the handler mock for testing

--- tests/test_sender.py
+++ tests/test_sender.py
@@ -2,53 +2,8 @@ from unittest.mock import patch
 
 from src.sender import Sender
 
-defined_sender = "specific@example.com"
+from tests.mocks.sender_handler import MockHandler, defined_sender
 
-class MockHandler:
-    def __init__(self):
-        self.actions = {
-            defined_sender: ("accept", None)
-        }
-
-        self.stash = {
-            defined_sender: [
-                ("a message", ["a@b.c", "d@e.f"]),
-                ("a message", ["a@b.c", "d@e.f"])
-            ]
-        }
-
-    def get_action_for_sender(self, sender: str):
-        if sender in self.actions:
-            return self.actions[sender]
-        else:
-            return None
-
-    def get_patterns(self):
-        return iter([
-            (r".*@example\.com", "confirm", "foo"),
-            (r".*@nowhere\.example\.com", "reject", None)
-        ])
-
-    def set_action_for_sender(self, sender: str, action: str, ref: str):
-        self.actions[sender] = (action, ref)
-
-    def stash_email_for_sender(self, sender: str, msg: str, recipients: list[str]):
-        data = (msg, recipients)
-
-        if sender in self.stash:
-            self.stash[sender].append(data)
-        else:
-            self.stash[sender] = [data]
-
-    def unstash_emails_for_sender(self, sender: str):
-        if sender in self.stash:
-            emails = self.stash[sender]
-            del self.stash[sender]
-        else:
-            emails = []
-
-        for index, data in enumerate(emails):
-            yield (index, *data)
 
 @patch("src.sender.get_default_handler", side_effect=MockHandler)
 class TestSender:
