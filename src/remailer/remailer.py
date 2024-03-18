de57cd95bcefa3c81616b4834a38ee233da88a9d
Alistair MacDonald <Alistair.MacDonald@siriusopensource.com>
2024-03-18 18:11:41 +0000

    feat: add remailer code
    This makes use of the existing configuration keys

---
+++ src/remailer/remailer.py
@@ -0,0 +1,59 @@
+from smtplib import SMTP, SMTPServerDisconnected
+
+from config import Config
+
+
+class Remailer:
+    """
+    Provides a simple wrapper around the python mail code that is context
+    aware, allows for re-use and will default the envelope sender.
+
+    Configuration is via the following keys:
+    * `smtp_host` (defaults to "localhost") as the host to send via
+    * `smtp_port` (defaults to 25) as the port to connect to on the `smtp_host`
+    * `remailer_sender` (defaults to "<>") as the default envelope sender
+    """
+
+    def __init__(self, app_config: Config):
+        self.host = app_config.get("smtp_host", "localhost")
+        self.port = app_config.get("smtp_port", 25)
+        self.sender_from = app_config.get("remail_sender", "<>")
+
+        self.smtp = None
+
+    def get_connection(self) -> SMTP:
+        if not self._check_smtp_connection():
+            self._init_smtp_connection()
+
+        return self.smtp
+
+    def sendmail(self, recipients: list[str], message: str, sender: str = None) -> any:
+        connection = self.get_connection()
+        return connection.sendmail(sender or self.sender_from, recipients, message)
+
+    def __enter__(self) -> any:
+        return self
+
+    def __exit__(self, type, value, traceback) -> bool:
+        if self.smtp:
+            try:
+                self.smtp.quit()
+            except SMTPServerDisconnected:
+                self.smtp = None
+
+        return True
+
+    def _check_smtp_connection(self) -> bool:
+        if not self.smtp:
+            return False
+
+        try:
+            self.smtp.docmd("NOOP")
+            return True
+
+        except SMTPServerDisconnected:
+            self.smtp = None
+            return False
+
+    def _init_smtp_connection(self) -> None:
+        self.smtp = SMTP(host=self.host, port=self.port)
---
+++ tests/test_remailer.py
@@ -0,0 +1,84 @@
+from io import StringIO
+from os.path import dirname
+from smtplib import SMTPServerDisconnected
+from unittest.mock import Mock, patch
+
+import config
+import pytest
+
+from src.remailer.remailer import Remailer
+
+empty_cfg = config.Config(StringIO(""))
+cfg = config.Config(f"{dirname(__file__)}/fixtures/config.cfg")
+
+
+class TestRemailer:
+    def test_it_initialises_defaults(self):
+        mailer = Remailer(empty_cfg)
+
+        assert mailer.host == "localhost"
+        assert mailer.port == 25
+        assert mailer.sender_from == "<>"
+
+    def test_it_initialises_from_config(self):
+        mailer = Remailer(cfg)
+
+        assert mailer.host == cfg["smtp_host"]
+        assert mailer.port == cfg["smtp_port"]
+        assert mailer.sender_from == cfg["remail_sender"]
+
+    @patch("src.remailer.remailer.SMTP")
+    def test_it_provides_a_consistent_connection(self, mock_smtp):
+        mailer = Remailer(cfg)
+
+        connection = mailer.get_connection()
+
+        assert mailer.smtp is connection
+
+        new_connection = mailer.get_connection()
+
+        assert new_connection is connection
+
+    @patch("src.remailer.remailer.SMTP")
+    def test_it_handles_context(self, mock_smtp):
+        with Remailer(cfg) as mailer:
+            connection = mailer.get_connection()
+
+            assert mailer.smtp is connection
+
+    @pytest.mark.skip("Issue with the docmd mocking/connections being the same")
+    @patch("src.remailer.remailer.SMTP",)
+    def test_it_handles_disconnection(self, mock_smtp):
+        mock_smtp.return_value.docmd = Mock(side_effect=SMTPServerDisconnected)
+
+        mailer = Remailer(cfg)
+        mailer.get_connection()
+
+        mock_smtp.return_value.docmd.assert_not_called()
+
+        mailer.get_connection()
+
+        mock_smtp.return_value.docmd.assert_called_with("NOOP")
+
+    @patch("src.remailer.remailer.SMTP")
+    def test_it_defaults_the_sender(self, mock_smtp):
+        mailer = Remailer(cfg)
+
+        recipients = ["a@example.com", "b@example.com"]
+        message = "This is the dummy message"
+
+        mailer.sendmail(recipients, message)
+
+        mock_smtp.return_value.sendmail.assert_called_with(mailer.sender_from, recipients, message)
+
+    def test_the_sender_can_be_overridden(self):
+        with patch("src.remailer.remailer.SMTP", autospec=True) as mock_smtp:
+            mailer = Remailer(cfg)
+
+            sender = "someone@example.com"
+            recipients = ["a@example.com", "b@example.com"]
+            message = "This is the dummy message"
+
+            mailer.sendmail(recipients, message, sender)
+
+            mock_smtp.return_value.sendmail.assert_called_with(sender, recipients, message)
