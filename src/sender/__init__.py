7dde1adae8081b6730ea6140ec4cdd46eb46b680
Alistair MacDonald <Alistair.MacDonald@siriusopensource.com>
2024-04-10 10:26:39 +0100

    feat: Add a static entry handler for senders
    This works exclusively with the static tables.

--- src/sender/__init__.py
+++ src/sender/__init__.py
@@ -1,19 +1,29 @@
 from .handler_db import HandlerDb
+from .handler_db_static import HandlerDbStatic
 from .sender import Sender
 
-handlers = {"db": HandlerDb, "_default": "db"}
-handler = None
+handlers = {"db": HandlerDb, "static": HandlerDbStatic, "_default": "db"}
+instances = {}
+
+
+def get_handler_instance(name: str) -> any:
+    global instances
+
+    if name not in instances:
+        instances[name] = handlers[name]()
+
+    return instances[name]
 
 
 def get_default_handler() -> any:
-    global handler
+    handler_name = handlers.get("_default", "db")
 
-    if not handler:
-        handler_name = handlers.get("_default", "db")
-        handler = handlers[handler_name]()
-
-    return handler
+    return get_handler_instance(handler_name)
 
 
 def get_sender(email) -> Sender:
     return Sender(email, get_default_handler())
+
+
+def get_static_sender(email) -> Sender:
+    return Sender(email, get_handler_instance("static"))
---
+++ src/sender/handler_db_static.py
@@ -0,0 +1,174 @@
+import json
+import logging
+from typing import Iterable, Optional, Tuple
+
+from config import Config
+import psycopg
+
+from .typing import Action
+
+from src import services
+
+
+logger = logging.getLogger(__name__)
+
+
+class HandlerDbStatic:
+    def __init__(self, app_config: Config = None) -> None:
+        self.connection = services["db"] if "db" in services else None
+        self.app_config = app_config if app_config else services["app_config"]
+
+    def _get_connection(self) -> psycopg.Connection:
+        """
+        Return the database connection.
+        """
+        if not self.connection:
+            try:
+                self.connection = psycopg.connect(
+                    dbname=self.app_config.get("db.name", "postconfirm"),
+                    user=self.app_config.get("db.user", "postconfirm"),
+                    password=self.app_config.get("db.password", None),
+                    host=self.app_config.get("db.host", "localhost"),
+                    port=self.app_config.get("db.port", 5432)
+                )
+            except psycopg.OperationalError as e:
+                print(f"The error '{e}' occurred")
+                raise e
+
+        return self.connection
+
+    def get_action_for_sender(self, sender: str) -> Optional[Tuple[Action, str]]:
+        """
+        Return any action for the given sender
+        """
+        with self._get_connection().cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    action, ref
+                    FROM senders_static
+                    WHERE sender=%(sender)s AND type='E'
+                """,
+                {"sender": sender}
+            )
+            result = cursor.fetchone()
+
+            if result:
+                ref = result[1]
+                if ref:
+                    try:
+                        ref = json.loads(ref)
+                    except json.JSONDecodeError:
+                        pass
+
+                return (
+                    result[0],
+                    ref
+                )
+
+        return ('unknown', None)
+
+    def get_patterns(self) -> Iterable[Tuple[str, str, str]]:
+        """
+        Returns any pattern-type actions
+        """
+        with self._get_connection().cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    sender, action, ref
+                    FROM senders_static
+                    WHERE type='P'
+                """
+            )
+
+            yield cursor
+
+    def set_action_for_sender(self, sender: str, action: Action, ref: str) -> bool:
+        """
+        Sets the action for the sender
+        """
+        connection = self._get_connection()
+        with connection.cursor() as cursor:
+            encoded_ref = json.dumps(ref) if ref else None
+
+            try:
+                cursor.execute(
+                    """
+                    INSERT INTO senders_static
+                        (sender, action, ref, type, source)
+                        VALUES
+                            (%(sender)s, %(action)s, %(ref)s, 'E', 'static')
+                        ON CONFLICT (sender)
+                            DO UPDATE SET action=%(action)s
+                    """,
+                    {"sender": sender, "action": action, "ref": encoded_ref}
+                )
+                connection.commit()
+                return True
+
+            except Exception as e:
+                print(f"ERROR setting sender: {e}", flush=True)
+                return False
+
+    def stash_message_for_sender(
+        self, sender: str, msg: str, recipients: list[str]
+    ) -> bool:
+        """
+        Stores the message for the sender
+        """
+        connection = self._get_connection()
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(
+                    """
+                    INSERT INTO stash_static
+                        (sender, recipients, message)
+                        VALUES
+                            (%(sender)s, %(recipients)s, %(message)s)
+                    """,
+                    {"sender": sender, "recipients": json.dumps(recipients), "message": msg}
+                )
+                connection.commit()
+                return True
+
+            except Exception as e:
+                print(f"ERROR stashing mail: {e}")
+                return False
+
+    def unstash_messages_for_sender(
+        self, sender: str
+    ) -> Iterable[Tuple[str, list[str]]]:
+        """
+        Yields the messages for the sender
+        """
+        connection = self._get_connection()
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(
+                    """
+                    SELECT
+                        id, recipients, message
+                        FROM stash_static
+                        WHERE sender=%(sender)s
+                    """,
+                    {"sender": sender}
+                )
+
+                for (row_id, recipients, message) in cursor:
+                    yield (json.loads(recipients), message)
+
+                    # Use a different cursor to avoid clobbering the in-progress loop
+                    connection.cursor().execute(
+                        """
+                        DELETE FROM stash_static
+                            WHERE id=%(row_id)s
+                        """,
+                        {"row_id": row_id}
+                    )
+                    connection.commit()
+
+            except Exception as e:
+                print(f"ERROR unstashing mails: {e}", flush=True)
+                return
