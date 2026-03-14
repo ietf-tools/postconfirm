a1efe4564482d695f0f8e292af465a569a1a0567
dane foster <dene@slush.ca>
2026-03-14 14:31:40 +1300

    Add a boolean to validate smtp server certificates or not, as in most cases we're using the internal k8s service address which is unlikely to validate properly.

--- src/remailer/remailer.py
+++ src/remailer/remailer.py
@@ -15,6 +15,8 @@ class Remailer:
     Configuration is via the following keys:
     * `smtp_host` (defaults to "localhost") as the host to send via
     * `smtp_port` (defaults to 25) as the port to connect to on the `smtp_host`
+    * `smtp_validate_certs` (defaults to False) boolean as to whether to
+    validate the certificate presented by the smtp server
     * `remailer_sender` (defaults to "<>") as the default envelope sender
     * `smtp_username` (optional) username for SASL PLAIN authentication
     * `smtp_password` (optional) password for SASL PLAIN authentication
@@ -28,6 +30,7 @@ class Remailer:
         self.host = app_config.get("smtp_host", "localhost")
         self.port = app_config.get("smtp_port", 25)
         self.helo_host = app_config.get("smtp_helo_host", "localhost")
+        self.validate_certs = app_config.get("smtp_validate_certs", False)
         self.sender_from = app_config.get("remail_sender", "<>")
 
         self.username = app_config.get("smtp_username", None)
@@ -38,12 +41,17 @@ class Remailer:
                 "smtp_username and smtp_password must both be set or both be unset"
             )
 
-    async def sendmail(self, recipients: list[str], message: str, sender: str = None) -> any:
+    async def sendmail(
+        self, recipients: list[str], message: str, sender: str = None
+    ) -> any:
         if sender is None:
             sender = self.sender_from
         try:
             async with SMTP(
-                hostname=self.host, port=self.port, local_hostname=self.helo_host
+                hostname=self.host,
+                port=self.port,
+                local_hostname=self.helo_host,
+                validate_certs=bool(self.validate_certs),
             ) as smtp:
                 if self.username:
                     await smtp.starttls()
@@ -52,4 +60,3 @@ class Remailer:
         except Exception as e:
             logger.error("Exception in SMTP: %(reason)s", {"reason": str(e)})
             return False
-
