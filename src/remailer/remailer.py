3e4b5f46b70d810e8b0d206659929caad32bb852
dane foster <dene@slush.ca>
2026-03-14 15:49:02 +1300

    fix: actually boolean properly

--- src/remailer/remailer.py
+++ src/remailer/remailer.py
@@ -30,7 +30,16 @@ class Remailer:
         self.host = app_config.get("smtp_host", "localhost")
         self.port = app_config.get("smtp_port", 25)
         self.helo_host = app_config.get("smtp_helo_host", "localhost")
-        self.validate_certs = app_config.get("smtp_validate_certs", False)
+        if app_config.get("smtp_validate_certs", "False").lower() in [
+            "true",
+            "1",
+            "t",
+            "y",
+            "yes",
+        ]:
+            self.validate_certs = True
+        else:
+            self.validate_certs = False
         self.sender_from = app_config.get("remail_sender", "<>")
 
         self.username = app_config.get("smtp_username", None)
@@ -51,7 +60,7 @@ class Remailer:
                 hostname=self.host,
                 port=self.port,
                 local_hostname=self.helo_host,
-                validate_certs=bool(self.validate_certs),
+                validate_certs=self.validate_certs
             ) as smtp:
                 if self.username:
                     await smtp.starttls()
