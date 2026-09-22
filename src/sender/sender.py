cede13180768a644412a948fa1823512f67deba3
Dane Foster <dene@slush.ca>
2026-09-22 22:37:35 +1200

    Fix/strip batv (#80)
    * fix: strip batv if we find it befere checking sender address for actions
    
    * chore: bump chart version

--- src/sender/sender.py
+++ src/sender/sender.py
@@ -40,6 +40,12 @@ class Sender:
         """
         Return the sender email address.
         """
+
+        # strip BATV
+        if "=" in self.email and re.search("^[A-Za-z0-9-]+=[A-Za-z0-9-]+=[^=]+@", self.email):
+            logger.debug("BATV stripped from %(email)s", {"email": self.email})
+            self.email = re.sub("^[A-Za-z0-9-]+=[A-Za-z0-9-]+=", "", self.email)
+
         return self.email
 
     def get_action(self) -> Action:
